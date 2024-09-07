d007e758f710b4b6f5a45eb92dac9a0a79d4afb5
akiikoisoe <180363344+akiso999@users.noreply.github.com>
2024-09-07 23:11:35 +0900

    課題再提出

---
+++ src/FizzBuzz.java
@@ -0,0 +1,18 @@
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int number = 1; number <= 100; number++) { 
+			if ( number % 3 == 0 && number % 5 == 0 ) {
+				System.out.println( "FizzBuzz");
+			}else if (number % 3 == 0 ) {
+				System.out.println( "Fizz");
+			}else if (number % 5 == 0) {
+				System.out.println( "Buzz");
+			}else
+			System.out.println(number);
+		}
+			
+	}
+		
+}
